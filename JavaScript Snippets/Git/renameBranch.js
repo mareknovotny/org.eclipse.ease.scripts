1ed2029dd714ad970e3b17ee3aa66d346dc66d81
Max Hohenegger <eclipse@hohenegger.eu>
2016-10-30 18:37:55 +0100

    Bug 507360: Example Scripts for EGit integration
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=507360
    Change-Id: I6c0bb33c77a3d8ca3d8197567c5f9d55a6b0df2f
    Signed-off-by: Max Hohenegger <eclipse@hohenegger.eu>

---
+++ JavaScript Snippets/Git/renameBranch.js
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (C) 2016, Max Hohenegger <eclipse@hohenegger.eu>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+// name			: Rename Branch
+// popup		: enableFor(org.eclipse.egit.ui.internal.repository.tree.RepositoryNode)
+// description	: Do something with a repository
+loadModule("/System/Platform")
+loadModule('/System/UI')
+
+loadModule("/System/Git UI")
+
+renameBranch(getRepository(getSelection()));
